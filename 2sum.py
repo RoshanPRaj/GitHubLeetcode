a2e4beb4afc88daf034df7440b6279f3c9b7dbc3
Roshan Prabakar raj <roshanprabakarraj@gmail.com>
2018-07-25 22:48:55 -0400

    1st

---
+++ 2sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        search_dict = {}
+        for index,value in enumerate(nums):
+            if target - value in search_dict:
+                return search_dict[target-value], index
+            search_dict[value] = index
