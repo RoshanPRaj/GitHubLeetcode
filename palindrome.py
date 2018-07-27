5c07b3b0c4e7d819005fc1ac527e22063232a1ae
Roshan Prabakar raj <roshanprabakarraj@gmail.com>
2018-07-27 19:17:41 -0400

    1st

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        temp = x
+        rev = 0
+        final = 0
+        while (temp > 0):
+            final = temp % 10
+            rev = (rev * 10) + final
+            temp = temp / 10
+
+        if rev == x:
+            return True
+        else:
+            return False
