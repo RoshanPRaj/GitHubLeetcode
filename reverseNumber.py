b61f2f638064d5e04d2a9e20fb88c1e3f52c49fd
Roshan Prabakar raj <roshanprabakarraj@gmail.com>
2018-07-26 20:39:02 -0400

    1st try

---
+++ reverseNumber.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        temp_num = 0
+        if x == 0:
+            return 0
+        if x < 0:
+            x = x * -1
+            while (x>0):
+                temp_num = (temp_num*10) + (x % 10)
+                x = x/10
+            return -1 * temp_num
+
+        else:
+            while (x>0):
+                temp_num = (temp_num*10) + (x % 10)
+                x = x/10
+            return temp_num
+        x = int(str(x))
+        if x not in range(-2147483648,2147483647):
+            return 0
