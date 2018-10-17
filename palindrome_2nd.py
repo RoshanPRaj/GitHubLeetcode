a4cc2d2d1b08c35b48887c20762f37b043458d2a
RoshanPRaj <41704694+RoshanPRaj@users.noreply.github.com>
2018-10-17 10:17:02 -0400

    Create Palindrome2nd approach

---
+++ palindrome_2nd.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        rev = 0
+        temp = x
+        while(temp > 0):
+            rev = rev*10 + (temp % 10)
+            temp = temp / 10
+        if rev == x:
+            return True
+        return False
