8d64aa377d5a7ebaa36d8476ac530f86528b377f
RoshanPRaj <41704694+RoshanPRaj@users.noreply.github.com>
2018-10-17 10:05:15 -0400

    Create palindrome_1.py

---
+++ palindrome_1.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        else:
+            y = float(str(x)[::-1].strip('0'))
+        if y == x:
+            return True
+        else:
+            return False
