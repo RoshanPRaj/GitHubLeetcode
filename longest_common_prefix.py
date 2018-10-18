4776dff92b343294f4855cec8da2f6cedd516138
RoshanPRaj <41704694+RoshanPRaj@users.noreply.github.com>
2018-10-18 11:33:14 -0400

    create longest_common_prefix

---
+++ longest_common_prefix.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        strs.sort()
+        a = strs[0]
+        b = strs[-1]
+        print a + b
+        minl = min(len(a), len(b))
+        s = ''
+        for i in range(minl):
+            if a[i] != b[i]:
+                break
+            s = s + a[i]
+        return s
